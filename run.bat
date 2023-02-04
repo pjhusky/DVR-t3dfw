e5b90eed2668ed382d78cfbb6a72cb67cda83e82
P H <p.houska@cgv.tugraz.at>
2023-02-04 21:39:38 +0100

    had forgotten to adapt run.bat to new mingw and debug build naming schemes to keep things separated

--- run.bat
+++ run.bat
@@ -4,11 +4,11 @@ set ARGS=-x 1600 -y 1200
 @IF "%~1" == "debug" GOTO :runDebug
 
 :runRelease
-bin\Win64\ReleaseMingw\T3DFW_DVR_Project.exe %ARGS%
+bin\Win64\Release_mingw\T3DFW_DVR_Project.exe %ARGS%
 GOTO exitRun
 
 :runDebug
-bin\Win64\DebugMingw\T3DFW_DVR_Project.exe %ARGS%
+bin\Win64\Debug_mingw\T3DFW_DVR_Project_d.exe %ARGS%
 
 :exitRun
 @exit /b %errorlevel%

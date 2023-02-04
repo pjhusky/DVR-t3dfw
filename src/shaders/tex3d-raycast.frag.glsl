9c07388424f1d2fc11b088d18afca7e4ec002990
P H <p.houska@cgv.tugraz.at>
2023-02-04 05:51:16 +0100

    use shader includes

--- src/shaders/tex3d-raycast.frag.glsl
+++ src/shaders/tex3d-raycast.frag.glsl
@@ -1,5 +1,7 @@
 #version 330 core
 
+#extension GL_GOOGLE_include_directive : enable
+
 in vec3 v_ndcPos;
 in vec3 v_coord3d;
 layout ( location = 0 ) out vec4 o_fragColor;
@@ -21,37 +23,26 @@ uniform vec4 u_camPos_OS;
 uniform vec3 u_volDimRatio;
 uniform float u_recipTexDim;
 
-#if 0
-vec3 rand01(inout uvec3 x){                   // pseudo-random number generator
-    for (int i=3; i-->0;) x = ((x>>8U)^x.yzx)*1103515245U;
-    return vec3(x)*(1.0/float(0xffffffffU));
-}
-#else
-// http://www.jcgt.org/published/0009/03/02/
-// https://www.shadertoy.com/view/XlGcRh
-vec3 rand01( inout uvec3 v ) {
-    v = v * 1664525u + 1013904223u;
 
-    v.x += v.y*v.z;
-    v.y += v.z*v.x;
-    v.z += v.x*v.y;
-
-    v ^= ( v >> 16u );
-
-    v.x += v.y*v.z;
-    v.y += v.z*v.x;
-    v.z += v.x*v.y;
-
-    vec3 fval = vec3( v.x, v.y, v.z );
-    return fval * ( 1.0f / 0xffffffffU );
-}
-#endif
-
-//float RANDOM( inout int g_seed ) {
-//    g_seed = (214013 * g_seed + 2531011);
-//    return ( (g_seed >> 16) & 0x7FFF / 66635.0 );
-//}
+#include "shaderUtils.h.glsl"
+//vec3 rand01( inout uvec3 v ) {
+//    v = v * 1664525u + 1013904223u;
 //
+//    v.x += v.y*v.z;
+//    v.y += v.z*v.x;
+//    v.z += v.x*v.y;
+//
+//    v ^= ( v >> 16u );
+//
+//    v.x += v.y*v.z;
+//    v.y += v.z*v.x;
+//    v.z += v.x*v.y;
+//
+//    vec3 fval = vec3( v.x, v.y, v.z );
+//    return fval * ( 1.0f / 0xffffffffU );
+//}
+
+
 
 // intersect ray with a box
 // http://www.siggraph.org/education/materials/HyperGraph/raytrace/rtinter3.htm

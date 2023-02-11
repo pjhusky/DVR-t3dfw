ee21c27650571b339c6b02110597ae9baa008a1d
P H <p.houska@cgv.tugraz.at>
2023-02-11 12:49:41 +0100

    git-pull-all script updated so that it works with master & main branch naming conventions

--- git-pull-all.bat
+++ git-pull-all.bat
@@ -1,31 +1,35 @@
 @echo.
-@echo ######### STEP 1 / 6 ############
-@REM git submodule foreach --recursive git pull origin main
-@REM git submodule foreach --recursive git pull origin master
-git submodule foreach --recursive git pull origin
+@echo ######### START REPO UPDATE ###########
 
 @echo.
-@echo ######### STEP 2 / 6 ############
-git pull
+@echo ######### STEP 1 / 5 ############
+git submodule foreach --recursive git submodule update --init --remote --merge
 
 @echo.
-@echo ######### STEP 3 / 6 ############
+@echo ######### STEP 2 / 5 ############
 @echo checked-out submodules may be in detached HEAD state now - fixing this now!
-
-@REM # create new branch (saving commits made in detached head state)
 git submodule foreach --recursive git branch temp-merge
 
 @echo.
-@echo ######### STEP 4 / 6 ############
-@REM # checkout main (no commits are lost! -- otherwise commits made in detached head state can be seen with 'git reflog'
-git submodule foreach --recursive git checkout main
+@echo ######### STEP 3 / 5 ############
+@REM git submodule foreach --recursive git stash
+@REM git submodule foreach --recursive git checkout (master 2>/dev/null || git checkout main)
+@REM git submodule foreach --recursive "git checkout master ; git checkout main ; echo done"
+@REM 2>/dev/null redirects error msg to null device, since we are fine with master not-being-found errors
+git submodule foreach --recursive "git checkout master 2>/dev/null || git checkout main ; echo done"
+@REM git submodule foreach --recursive git stash pop
 
 @echo.
-@echo ######### STEP 5 / 6 ############
+@echo ######### STEP 4 / 5 ############
 @REM # merge commits made locally in detached head state that are now in temp-merge branch back into main
 git submodule foreach --recursive git merge temp-merge
 
 @echo.
-@echo ######### STEP 6 / 6 ############
+@echo ######### STEP 5 / 5 ############
 @REM # delete the temp branch after the merge back to main as it is no longer needed
 git submodule foreach --recursive git branch --delete temp-merge
+
+@echo.
+@echo #########  REPO UP-TO-DATE  ###########
+@echo.
+

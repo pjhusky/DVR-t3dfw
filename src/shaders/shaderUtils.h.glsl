cc61179d656b262f7e0b156a8fe3a06536822346
P H <p.houska@cgv.tugraz.at>
2023-02-13 19:02:54 +0100

    added 'banding' for testing: rand01 always returns 0.5

--- src/shaders/shaderUtils.h.glsl
+++ src/shaders/shaderUtils.h.glsl
@@ -1,28 +1,30 @@
 
 #if 0
-vec3 rand01(inout uvec3 x){                   // pseudo-random number generator
-    for (int i=3; i-->0;) x = ((x>>8U)^x.yzx)*1103515245U;
-    return vec3(x)*(1.0/float(0xffffffffU));
-}
+    vec3 rand01(inout uvec3 x) {                   // pseudo-random number generator
+        for (int i=3; i-->0;) x = ((x>>8U)^x.yzx)*1103515245U;
+        return vec3(x)*(1.0/float(0xffffffffU));
+    }
+#elif 0 // DEBUG
+    vec3 rand01(inout uvec3 x) { return vec3( 0.5 ); }
 #else
-// http://www.jcgt.org/published/0009/03/02/
-// https://www.shadertoy.com/view/XlGcRh
-vec3 rand01( inout uvec3 v ) {
-    v = v * 1664525u + 1013904223u;
+    // http://www.jcgt.org/published/0009/03/02/
+    // https://www.shadertoy.com/view/XlGcRh
+    vec3 rand01( inout uvec3 v ) {
+        v = v * 1664525u + 1013904223u;
 
-    v.x += v.y*v.z;
-    v.y += v.z*v.x;
-    v.z += v.x*v.y;
+        v.x += v.y*v.z;
+        v.y += v.z*v.x;
+        v.z += v.x*v.y;
 
-    v ^= ( v >> 16u );
+        v ^= ( v >> 16u );
 
-    v.x += v.y*v.z;
-    v.y += v.z*v.x;
-    v.z += v.x*v.y;
+        v.x += v.y*v.z;
+        v.y += v.z*v.x;
+        v.z += v.x*v.y;
 
-    vec3 fval = vec3( v.x, v.y, v.z );
-    return fval * ( 1.0f / 0xffffffffU );
-}
+        vec3 fval = vec3( v.x, v.y, v.z );
+        return fval * ( 1.0f / 0xffffffffU );
+    }
 #endif
 
 // intersect ray with a box

c764164ef7aa3062aaf4dda452039936d2c97858
P H <p.houska@cgv.tugraz.at>
2023-02-25 20:39:54 +0100

    minor cleanup of DVR shader (numPosDensities not used)

--- src/shaders/tex3d-raycast.frag.glsl
+++ src/shaders/tex3d-raycast.frag.glsl
@@ -70,9 +70,6 @@ void main() {
     end_sample_pos = end_sample_pos / u_volDimRatio * 0.5 + 0.5;
 
     vec4 color = vec4( 0.0 );
-#if ( DVR_MODE == XRAY )
-    float numPosDensities = 0.0;
-#endif
     
     //uvec2 pix = uvec2( uint( gl_FragCoord.x ), uint( gl_FragCoord.y ) );
     uvec2 pix = uvec2( uint( gl_FragCoord.x + 0.17 * u_frameNum ), uint( gl_FragCoord.y + 0.17 * u_frameNum ) );
@@ -221,7 +218,6 @@ void main() {
         color.a   = color.a + currBlendFactor;
     #elif ( DVR_MODE == XRAY )
         color += vec4( colorAndAlpha.rgb * colorAndAlpha.a, colorAndAlpha.a );
-        numPosDensities += ( colorAndAlpha.a > 0.0001 ) ? 1.0 : 0.0;
     #elif ( DVR_MODE == MRI )
         if ( colorAndAlpha.a > color.a ) {
             color.a = colorAndAlpha.a;
@@ -245,9 +241,6 @@ void main() {
 
     #if ( DVR_MODE == XRAY )
         color = ( color - u_minMaxScaleVal.x ) * u_minMaxScaleVal.z; // map volume-tex values to 0-1 after the loop
-        //o_fragColor.rgb = color.rgb / numPosDensities;
-        //o_fragColor.rgb = color.rgb * ( numPosDensities * ( lenInVolume / RAY_STEP_SIZE ) );
-        //o_fragColor.rgb = color.rgb / ( numPosDensities / color.a );
         o_fragColor.rgb = color.rgb / ( lenInVolume / RAY_STEP_SIZE );
     #else
         o_fragColor.rgb = color.rgb * lightColor.rgb;        
